18372ce9635bd9348e53cc2378223feb8bdf2ba3
samin1994 <saminlabbaf1994@gmail.com>
2025-04-26 18:04:35 -0400

    initial features and curve

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HeroComponent } from './components/hero/hero.component';
+import { FeaturesMainComponent } from './components/features-main/features-main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HeroComponent
+    HeroComponent,
+    FeaturesMainComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/features-main/features-main.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-features-main',
+  standalone: false,
+  templateUrl: './features-main.component.html',
+  styleUrl: './features-main.component.scss'
+})
+export class FeaturesMainComponent {
+
+}
